04e9979f714fcc0c7b0a1a5c42980b91a66e7670
Diego Sanchez <sanchez.49@wright.edu>
2025-11-04 21:53:12 -0500

    started html; stuck at erros with @for loop

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WatchedMovieListComponent } from "./watched-movie-list.component/watched-movie-list.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, WatchedMovieListComponent, WatchedMovieListComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- src/app/watched-movie-list.component/watched-movie-list.component.ts
+++ src/app/watched-movie-list.component/watched-movie-list.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+
+interface WatchedMovie{
+  name: string;
+  id: number;
+  watchedDate: string;
+}
 
 @Component({
-  selector: 'app-watched-movie-list.component',
-  imports: [],
+  selector: 'app-watched-movie-list',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './watched-movie-list.component.html',
   styleUrl: './watched-movie-list.component.scss',
 })
 export class WatchedMovieListComponent {
+
+  // Recreation of DB for watched movies on user `jdoe` user page
+  // movieID is now id
+  // user is now name
+  watchedMovies = signal<WatchedMovie[]>([ // Should be set to import DTO later (I think)
+    {id: 1, name: 'jdoe', watchedDate: '2023-05-01'},
+    {id: 2, name: 'jdoe', watchedDate: '2023-05-05'}
+  ]);
 
 }
