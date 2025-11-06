43f0fa054f5d2031ac22aeba11c4fe4b01e23421
Jerico Corneja <jerico.corneja20@gmail.com>
2025-11-05 21:37:26 -0500

    add @for and styling

---
+++ src/app/shared/components/user-list/user-list.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-user-list',
+  imports: [CommonModule],
+  templateUrl: './user-list.html',
+  styleUrl: './user-list.scss',
+})
+/**
+ * The Component class
+ *  Contains business logic and data live
+ */
+export class UserList {
+  // Data - Class Property (can be accessed in component and template)
+  users: User[] = [
+    {id: '1', name: "asmith" },
+    {id: '2', name: "bwayne" },
+    {id: '3', name: "ckent" },
+    {id: '4', name: "jdoe" },
+    {id: '5', name: "tony_stark123" }
+  ]
+}
+
+export interface User {
+  id: string;
+  name: string;
+}
