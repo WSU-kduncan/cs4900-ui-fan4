9ef571fa169a175d03a12d8b580cef78dad72ada
Diego Sanchez <sanchez.49@wright.edu>
2025-11-06 10:10:16 -0500

    switched array to standard array (not signal)

--- src/app/watched-movie-list.component/watched-movie-list.component.ts
+++ src/app/watched-movie-list.component/watched-movie-list.component.ts
@@ -19,9 +19,9 @@ export class WatchedMovieListComponent {
   // Recreation of DB for watched movies on user `jdoe` user page
   // movieID is now id
   // user is now name
-  watchedMovies = signal<WatchedMovie[]>([ // Should be set to import DTO later (I think)
+  watchedMovies: WatchedMovie[] =[ // Should be set to import DTO later (I think)
     {id: 1, name: 'jdoe', watchedDate: '2023-05-01'},
     {id: 2, name: 'jdoe', watchedDate: '2023-05-05'}
-  ]);
+  ];
 
 }
