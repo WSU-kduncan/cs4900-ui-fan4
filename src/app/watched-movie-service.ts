aa180c950fafc08a985a5b2186a8d5b78723a9ff
Diego Sanchez <sanchez.49@wright.edu>
2025-11-12 23:29:28 -0500

    updated watchedMovie service

--- src/app/watched-movie-service.ts
+++ src/app/watched-movie-service.ts
@@ -1,8 +1,22 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
+import { WatchedMovieListComponent } from './watched-movie-list.component/watched-movie-list.component';
+
+export interface WatchedMovie{
+  name: string;
+  id: number;
+  watchedDate: string;
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class WatchedMovieService {
-  
+  watchedMovies = signal<WatchedMovie[]>([
+    {id: 1, name: 'jdoe', watchedDate: '2023-05-01'},
+    {id: 2, name: 'jdoe', watchedDate: '2023-05-05'}
+  ]);
+
+  addWatchedMovie(newWatchedMovie : WatchedMovie){
+    this.watchedMovies.update((currentWatchedMovies) => [...currentWatchedMovies, newWatchedMovie]);
+  }
 }
