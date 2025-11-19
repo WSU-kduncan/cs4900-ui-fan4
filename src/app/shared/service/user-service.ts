b10d944035a1231c4acabac70197506b9266a8e4
Jerico Corneja <jerico.corneja20@gmail.com>
2025-11-18 23:06:13 -0500

    create a signal to use in components

--- src/app/shared/service/user-service.ts
+++ src/app/shared/service/user-service.ts
@@ -1,8 +1,19 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
+import { User } from '../models/user.model';
 
+
+// This allows UserService to be available application-wide
+// Also allows the service to declare its own scope
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  
+  // Data - Class Property (can be accessed in component and template)
+  users = signal<User[]>([
+    {id: '1', name: "asmith" },
+    {id: '2', name: "bwayne" },
+    {id: '3', name: "ckent" },
+    {id: '4', name: "jdoe" },
+    {id: '5', name: "tony_stark123" }
+  ]);
 }
