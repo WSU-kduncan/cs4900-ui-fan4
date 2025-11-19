0ec32742d8ee6c21605894f58e80b6e51784f82f
Jerico Corneja <jerico.corneja20@gmail.com>
2025-11-18 23:48:19 -0500

    provide service to app

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,13 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { UserService } from './shared/service/user-service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    UserService
   ]
 };
