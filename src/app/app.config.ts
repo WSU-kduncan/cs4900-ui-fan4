9ed459347ac8d446a9f6f521e6adb4adf594c3d5
Jerico Corneja <jerico.corneja20@gmail.com>
2025-11-20 16:04:23 -0500

    Configure HttpClient

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,12 +3,14 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { UserService } from './shared/service/user-service';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    UserService
+    UserService,
+    provideHttpClient()
   ]
 };
