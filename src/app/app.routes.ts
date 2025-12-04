de07c648826a8d5c1c8c9828c1819669a63665f8
diego <sanchez.49@wright.edu>
2025-12-04 01:18:57 -0500

    routes updated for login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,24 @@
 import { Routes } from '@angular/router';
 import { HomePage } from './home-page/home-page';
 import { UserReviewList } from './user-review-list/user-review-list';
+import { LoginPage } from './user-login-page/login-page';
 
 export const routes: Routes = [
   { path: '', component: HomePage },
   { path: 'movie/:id', component: UserReviewList },
+  { path: 'login', component: LoginPage},
+
+
+    // Default redirect
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+
+  // Wildcard route - must be last
+  {
+    path: '**',
+    redirectTo: '/login'
+  }
 ];
